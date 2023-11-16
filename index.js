0fc4b2e19755fc74a6970d7efc56f7539105645e
zhangze <1183460943@qq.com>
2023-11-16 11:33:58 +0800

    fix: fix tag color

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
 const express = require('express');
+const path = require('path')
 
 const app = express();
 
-app.use(express.static('./static'));
+app.use(express.static(path.join(__dirname, 'static')));
 
-
-app.listen(3000)
+app.listen(3000, '0.0.0.0')
